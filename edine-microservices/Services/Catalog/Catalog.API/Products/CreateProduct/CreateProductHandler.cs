c5d5cb48af29d04b8223eb9f033b986ff7744519
David <davidoyekanmi.t@gmail.com>
2025-07-15 13:46:19 +0100

    FluentValidation

--- edine-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ edine-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,32 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, decimal Price, string? Description, string? Image)
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
-internal class CreateProductHandler(IDocumentSession session)
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+internal class CreateProductHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+        
+        if (errors.Any())
+            throw new ValidationException(errors.FirstOrDefault());
+
         Product product = new Product
         {
             Name = command.Name,
