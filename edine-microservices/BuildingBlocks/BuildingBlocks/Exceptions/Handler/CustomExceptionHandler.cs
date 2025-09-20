71422edcf1386197a2730a10049046d3b3217ffb
David <davidoyekanmi.t@gmail.com>
2025-09-20 07:33:35 +0100

    Implented Loggin and Custom Exception Handler

---
+++ edine-microservices/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,68 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.Exceptions.Handler;
+
+public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.LogError(
+            "Error Message: {exceptionMessage}, Time of occurence {time}",
+            exception.Message, DateTime.Now);
+
+        (string Detail, string Title, int StatusCode) details = exception switch
+        {
+            InternalServerException => (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+
+            ),
+            ValidationException => (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            BadRequestException => (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            NotFoundException => (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status404NotFound
+            ),
+            _ => (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            )
+        };
+
+        var response = new ProblemDetails
+        {
+            Title = details.Title,
+            Status = details.StatusCode,
+            Detail = details.Detail,
+            Instance = context.Request.Path
+        };
+
+        response.Extensions.Add("traceId", context.TraceIdentifier);
+
+        if (exception is ValidationException validationException)
+        {
+            response.Extensions.Add("ValidationErrors", validationException.Message);
+        }
+
+        await context.Response.WriteAsJsonAsync(response, cancellationToken);
+
+        return true;
+    }
+}
