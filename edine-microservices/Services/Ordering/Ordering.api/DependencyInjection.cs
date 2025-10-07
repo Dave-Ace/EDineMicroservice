8ebc9f9cbbefe6eea8bef7c8321cc12783524bd7
David <davidoyekanmi.t@gmail.com>
2025-10-07 23:46:25 +0100

    Dependency Injection for each Architecture Layer

---
+++ edine-microservices/Services/Ordering/Ordering.api/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAPIServices(this IServiceCollection services)
+    {
+        //services.Carter();
+
+        return services;
+    }
+
+    public static WebApplication UseAPIServices(this WebApplication app)
+    {
+        //app.MapCarter();
+
+        return app;
+    }
+}
