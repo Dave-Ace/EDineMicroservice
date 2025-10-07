7695bf42b53d079db47836eb95d4b2d55bd4902e
David <davidoyekanmi.t@gmail.com>
2025-10-07 23:46:48 +0100

    Registering/Injecting the services in Program.cs

--- edine-microservices/Services/Ordering/Ordering.api/Program.cs
+++ edine-microservices/Services/Ordering/Ordering.api/Program.cs
@@ -1,6 +1,15 @@
+using Ordering.api;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddAPIServices();
 
 var app = builder.Build();
 
