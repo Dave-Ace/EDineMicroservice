808515babd73ce327f8d3f52c5baa3d91ceaa579
David <davidoyekanmi.t@gmail.com>
2025-10-08 21:13:15 +0100

    Order Model

--- edine-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ edine-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -8,7 +8,7 @@ namespace Ordering.Domain.Models;
 public class Order : Aggregate<Guid>
 {
     private readonly List<OrderItem> _orderItems = new();
-    public IReadOnlyList<OrderItem> OrderItems = _orderItems.AsReadOnly();
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
 
     public Guid CustomerId { get; private set; } = default;
     public string OrderName { get; private set; } = default;
@@ -16,4 +16,9 @@ public class Order : Aggregate<Guid>
     public Address BillingAddress { get; private set; } = default;
     public Payment Payment {get; private set;} = default;
     public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
+    }
 }
