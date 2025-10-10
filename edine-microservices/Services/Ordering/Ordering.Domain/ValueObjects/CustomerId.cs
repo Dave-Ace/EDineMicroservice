261e5e1e0e5e744f49e9b2ae53519cd59b58d222
David <davidoyekanmi.t@gmail.com>
2025-10-10 20:55:55 +0100

    OrderItemID Rich Domain Entity

--- edine-microservices/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ edine-microservices/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -4,9 +4,7 @@ namespace Ordering.Domain.ValueObjects;
 public record CustomerId
 {
     public Guid Value { get; }
-
     private CustomerId(Guid value) => Value = value;
-
     public static CustomerId Of(Guid value)
     {
         ArgumentNullException.ThrowIfNull(value);
--- edine-microservices/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
+++ edine-microservices/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
@@ -4,4 +4,16 @@ namespace Ordering.Domain.ValueObjects;
 public record OrderItemId
 {
     public Guid Value { get; }
+    public OrderItemId(Guid value) => Value = Value;
+
+    public static OrderItemId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("OrderItemId cannot be empty.");
+        }
+
+        return new OrderItemId(value);
+    }
 }
