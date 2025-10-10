2b0e7e4567eba17952ba043e0f75fd0f98edf8ab
David <davidoyekanmi.t@gmail.com>
2025-10-10 20:10:00 +0100

    Update entity to reflect the strong typed object

--- edine-microservices/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ edine-microservices/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ordering.Domain.Abstractions;
-
+﻿
 namespace Ordering.Domain.Models;
 
-public class Customer : Entity<Guid>
+public class Customer : Entity<CustomerId>
 {
     public string Name { get; private set; } = default!;
     public string Email { get; private set; } = default!;
