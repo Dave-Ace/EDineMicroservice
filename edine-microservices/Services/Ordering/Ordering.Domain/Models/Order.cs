b61159954ddbc288e5438a18caab3dccb3dfbf83
David <davidoyekanmi.t@gmail.com>
2025-10-11 20:34:03 +0100

    Order Create

--- edine-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ edine-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -17,4 +17,20 @@ public class Order : Aggregate<OrderId>
         get => OrderItems.Sum(x => x.Price * x.Quantity);
         private set { }
     }
+
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending,
+        };
+
+        return order;
+    }
 }
