4521b7f2905e691074b6afd0408bc6130bc6b97a
David <davidoyekanmi.t@gmail.com>
2025-10-12 21:29:05 +0100

    chore

--- edine-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ edine-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -32,7 +32,7 @@ public class Order : Aggregate<OrderId>
         };
 
 
-        //order.AddDomainEvent(new OrderCreatedEvent(order));
+        //order.AddDomainEvent(new OrderCreatedEvent(order));s
         
         return order;
     }
